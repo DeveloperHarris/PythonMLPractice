2af0d9a80d6f53f5c314928ab927002ca8ad08e0
Harris | Vortex <harris.rothaermel@gmail.com>
2016-12-24 15:43:13 -0500

    Improved Twitterbot and worked on visualization.
    Visualize.py is practice for dimensional reduction of high dimensional
    data.

--- twitterbot.py
+++ twitterbot.py
@@ -3,11 +3,11 @@ from textblob import TextBlob
 
 
 #authorization info
-consumer_key = 'foo.bar'
-consumer_secret = 'foo.bar'
+consumer_key = 
+consumer_secret = 
 
-access_token = 'foo.bar'
-access_token_secret =  'foo.bar'
+access_token = 
+access_token_secret =  
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -15,11 +15,16 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 #search public tweets containing this string
-public_tweets = api.search('Politics')
+public_tweets = api.search(input('Please enter your search query: '))
 
 #print out the tweets found, and their sentiment analysis
 for tweet in public_tweets:
 	print(tweet.text.encode("utf-8"))
-	analysis = TextBlob(tweet.text)
+	print('Author: ' + str(tweet.user.screen_name))
+	print('Created: ' + str(tweet.created_at))
+	print('Location: ' + str(tweet.place))
+	print('Favorites: ' + str(tweet.favorite_count))
+	print('Retweets: ' + str(tweet.retweet_count))
+	analysis = TextBlob(tweet.text) 
 	print(analysis.sentiment)
 	print('\n')
